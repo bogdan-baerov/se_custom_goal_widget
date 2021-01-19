0cec96bc5eafd7a979a1b46202beb0698e0fc07a
Baerov <baerovbogdan@gmail.com>
2021-01-19 17:38:02 +0200

    init commit

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# SE_CustomGoalWidget
 
 I used the custom code & documentation from here, and then I updated and made my own:
 https://github.com/StreamElements/widgets/blob/master/CustomCode.md
@@ -9,5 +8,9 @@ Usage:
 - Create or open an existent StreamElements Scene;
 - Click on the +, select "Static / Custom", then "Custom Widget";
 - Click on "Open Editor" on the sidebar in the left;
-- Copy paste the code in each file in their own tab(e.g:code.html goes in the "Html" tab and so on);
+- Copy paste the code in each file in their own tab:
+    o code.html goes in the "HTML" tab;
+    o code.css goes in the "CSS" tab;
+    o code.js goes in the "JS" tab;
+    o code.json goes in the "FIELDS" tab;
 - Customize as you wish.
---
+++ code.js
@@ -0,0 +1,110 @@
+let index, goal, fieldData, currency, userLocale, prevCount, timeout;
+
+function setGoal() {
+    if (fieldData['eventType'] === 'tip') {
+        if (goal % 1) {
+            $("#goal").html(goal.toLocaleString(userLocale, {style: 'currency', currency: currency}));
+        } else {
+            $("#goal").html(goal.toLocaleString(userLocale, {
+                minimumFractionDigits: 0,
+                style: 'currency',
+                currency: currency
+            }));
+        }
+    } else {
+        $("#goal").html(goal);
+    }
+}
+
+window.addEventListener('onWidgetLoad', async function (obj) {
+        fieldData = obj.detail.fieldData;
+        goal = fieldData["goal"];
+        userLocale = fieldData["userLocale"];
+        currency = obj["detail"]["currency"]["code"];
+        index = fieldData['eventType'] + "-" + fieldData['eventPeriod'];
+        if (fieldData['eventType'] === "subscriber-points") {
+            index = fieldData['eventType'];
+        }
+        let count = 0;
+        if (typeof obj["detail"]["session"]["data"][index] !== 'undefined') {
+            if (fieldData['eventPeriod'] === 'goal' || fieldData['eventType'] === 'cheer' || fieldData['eventType'] === 'tip' || fieldData['eventType'] === 'subscriber-points') {
+                count = obj["detail"]["session"]["data"][index]['amount'];
+            } else {
+                count = obj["detail"]["session"]["data"][index]['count'];
+            }
+        }
+        if (fieldData['botCounter']) {
+            goal = await getCounterValue(obj.detail.channel.apiToken);
+        }
+        setGoal()
+        updateBar(count);
+    }
+);
+
+let getCounterValue = apiKey => {
+    return new Promise(resolve => {
+        fetch("https://api.streamelements.com/kappa/v2/channels/me", {
+            "headers": {
+                "accept": "application/json, text/plain, */*",
+                "authorization": `apikey ${apiKey}`
+            }, "method": "GET"
+        }).then(response => response.json()).then(obj => {
+            fetch(`https://api.streamelements.com/kappa/v2/bot/${obj._id}/counters/goal`).then(response => response.json()).then(data => {
+                resolve(data.value)
+            })
+        });
+    })
+};
+
+window.addEventListener('onSessionUpdate', function (obj) {
+    if (typeof obj["detail"]["session"][index] !== 'undefined') {
+        if (fieldData['eventPeriod'] === 'goal' || fieldData['eventType'] === 'cheer' || fieldData['eventType'] === 'tip' || fieldData['eventType'] === 'subscriber-points') {
+            count = obj["detail"]["session"][index]['amount'];
+        } else {
+            count = obj["detail"]["session"][index]['count'];
+        }
+    }
+    updateBar(count);
+});
+
+window.addEventListener('onEventReceived', function (obj) {
+    const listener = obj.detail.listener;
+    const data = obj.detail.event;
+
+    if (listener === 'bot:counter' && data.counter === "goal") {
+        goal = data.value;
+        setGoal();
+        updateBar(count);
+    }
+});
+
+
+function updateBar(count) {
+    if (count === prevCount) return;
+    if (count >= goal && fieldData['autoIncrement'] > 0) {
+        goal += fieldData['autoIncrement'];
+        setGoal();
+        updateBar(count);
+        return;
+    }
+    clearTimeout(timeout);
+    prevCount = count;
+    $("body").fadeTo("slow", 1);
+    let percentage = Math.min(100, (count / goal * 100).toPrecision(3));
+    $("#bar").css('width', percentage + "%");
+  	$("#goalPercent").html(Math.trunc(percentage) + "%");
+    if (fieldData['eventType'] === 'tip') {
+        if (count % 1) {
+            count = count.toLocaleString(userLocale, {style: 'currency', currency: currency})
+        } else {
+            count = count.toLocaleString(userLocale, {minimumFractionDigits: 0, style: 'currency', currency: currency})
+        }
+    }
+    $("#count").html(count);
+    if (fieldData.fadeoutAfter) {
+        timeout = setTimeout(() => {
+            $("body").fadeTo("slow", 0);
+        }, fieldData.fadeoutAfter * 1000)
+    }
+  
+}
